da3bb6556d5d3a63a920cb1d9103279672236ca8
NLSon03 <nlson.2003.nguyen@gmail.com>
2024-10-21 12:13:21 +0700

    Initial commit

---
+++ src/test/java/com/qlda/nhom6/Nhom6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qlda.nhom6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nhom6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
